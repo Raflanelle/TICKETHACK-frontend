be04b03fc5ba486917df9fbadb264e36ed932e15
raphaelle <raflanelle@hotmail.com>
2024-07-03 15:54:32 +0200

    frontend avec booking

---
+++ booking.js
@@ -0,0 +1,54 @@
+//récupère et met à jour sur booking.html les données du get
+fetch(`http://localhost:3000/booking`).then(response => response.json())
+.then(data => {
+    document.querySelector('#validation').innerHTML = ``;
+    if (data.trajets) {
+       console.log(data.trajets)
+       document.querySelector('#validation').innerHTML += `
+       <h3>My bookings</h3>
+       `;
+        for (let i = 0; i < data.trajets.length; i++) {
+            let hours = new Date(data.trajets[i].date).getHours();
+            hours = String(hours).padStart(2, "0");
+            let minutes = new Date(data.trajets[i].date).getMinutes();
+            minutes = String(hours).padStart(2, "0");
+            let deadline = new Date(data.trajets[i].date).getTime();
+            deadline -= Date.now()
+            deadline = Math.floor(deadline /3600000)
+            if (deadline === 0) {
+                document.querySelector('#validation').innerHTML += `
+            <div id = 'avenir'>
+                <p class = 'destination2'>${data.trajets[i].departure}> ${data.trajets[i].arrival}</p>
+                <p class = 'hour2'>${hours}:${minutes}</p>
+                <p class = 'price2'>${data.trajets[i].price}€</p>
+                <p class = 'attente'>Bon voyage!</p>
+            </div>
+        `;
+            } else if (deadline < 0) {
+                document.querySelector('#validation').style.display = "none";
+                document.querySelector('#booking').style.display = "flex";
+            }
+            
+            
+            document.querySelector('#validation').innerHTML += `
+            
+
+            <div id = 'avenir'>
+                <p class = 'destination2'>${data.trajets[i].departure}> ${data.trajets[i].arrival}</p>
+                <p class = 'hour2'>${hours}:${minutes}</p>
+                <p class = 'price2'>${data.trajets[i].price}€</p>
+                <p class = 'attente'>Departure in ${deadline} hours</p>
+            </div>
+        `;
+        }
+        document.querySelector('#validation').innerHTML += `
+        <hr class = 'ligne'/>
+            <div id = 'enjoy'>
+                <span>enjoy your travel with Tickethack!</span>
+            </div> -->
+            `;
+    } else {
+        document.querySelector('#validation').style.display = "none";
+        document.querySelector('#booking').style.display = "flex";
+    }
+})
